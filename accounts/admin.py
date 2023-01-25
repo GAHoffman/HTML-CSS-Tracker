bbaf231e86c47f5d11be9ce766b82e986fe95a8c
Garrett Hoffman <son0hoff@gmail.com>
2023-01-25 09:52:54 -0800

    Final submission

--- accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# No Models were created, so there are no Models
-# to be registered with Admin
--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# Forms are for the users are not Model based,
-# so no Models are used in the Accounts app
